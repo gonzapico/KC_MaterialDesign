88c9fd08174c1c63d7c978bfd945af592b99bd67
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-10-15 19:51:05 +0200

    Added recycler view

--- app/src/main/java/xyz/gonzapico/kc_md/HomeActivity.java
+++ app/src/main/java/xyz/gonzapico/kc_md/HomeActivity.java
@@ -2,8 +2,14 @@ package xyz.gonzapico.kc_md;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 public class HomeActivity extends BaseKCActivity {
+
+  private RecyclerView rvCards;
   @Override public int getLayout() {
     return R.layout.activity_home;
   }
@@ -11,6 +17,21 @@ public class HomeActivity extends BaseKCActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setUpTabLayout();
+
+    setUpRecyclerView();
+  }
+
+  private void setUpRecyclerView() {
+    rvCards = (RecyclerView) findViewById(R.id.rvCards);
+    /***
+     * Layout manager:
+     *  - Linear
+     *  - Grid
+     *  - StaggeredGrid
+     */
+    rvCards.setLayoutManager(new GridLayoutManager(this, 2));
+    // rvCards.setLayoutManager(new LinearLayoutManager(this));
+    // rvCards.setLayoutManager(new StaggeredGridLayoutManager(2, 2));
   }
 
   private void setUpTabLayout() {
