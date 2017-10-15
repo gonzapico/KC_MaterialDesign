6b7d093d4636f520ddb4cda142e9b45ee73eb274
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-10-15 19:32:22 +0200

    Added tab layout

--- app/src/main/java/xyz/gonzapico/kc_md/HomeActivity.java
+++ app/src/main/java/xyz/gonzapico/kc_md/HomeActivity.java
@@ -1,12 +1,22 @@
 package xyz.gonzapico.kc_md;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseKCActivity {
+  @Override public int getLayout() {
+    return R.layout.activity_home;
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
+    setUpTabLayout();
+  }
+
+  private void setUpTabLayout() {
+    TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+    tabs.addTab(tabs.newTab().setText("Tab 1"));
+    tabs.addTab(tabs.newTab().setText("Tab 2"));
+    tabs.addTab(tabs.newTab().setText("Tab 3"));
   }
 }
